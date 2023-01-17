24bfa7f58fc760692608250699f5d03b80a0a138
SHIVAMELU <87264996+SHIVAMELU@users.noreply.github.com>
2023-01-17 10:02:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 10248430
-    API_HASH = "42396a6ff14a569b9d59931643897d0d"
+    APP_ID = 21675257
+    API_HASH = c355e903072870b1d79de084958ede97
     # the name to display in your alive message
-    ALIVE_NAME = "rishabh"
+    ALIVE_NAME = tobi
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "postgres://jrycfssc:fEZMYWiOcO9vB8cIq1_Z97GhW3TiTppJ@tiny.db.elephantsql.com/jrycfssc"
+    DB_URI = postgres://fpeaawyg:y7S1-Jm-z420BwuTZzaTQhGbzU17u9CT@queenie.db.elephantsql.com/fpeaawyg
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "your session"
+    STRING_SESSION = BQBiMZkARYYs2KjGJH45wDp27zavIbI1w7CEjEaY9pgQbwFEC2ZBcNwyOBplxIyIGNEKd-EcFRAUVKpxa9x1NbDBDG-ZCyOoS0waS6nMKU_TlMEf4eUV-zx-pHMo9rdgSBVcDQPP1V7W1zgeRE7yfBawtKRjT9IsdSdiD20puO_frB6-vXARxJa0iphSLU3iyushNPPyAC3YbFrWXjc17-95IxtM0tfIbRPOs5jMiRjCRxfa9MlIbDNr2z7CnH5k6j_Sz1_yMtmv-XnZcX7apXfuoLjuA3VugFQ4vKQrnuXGeJHkYP6mFv_JkYgS6HochPUKO5YEfwx1GVVYVxROllIlsBpoQgAAAAFFw-__AA
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "your token"
+    TG_BOT_TOKEN = 5446322651:AAFLDXBuLLpCkroBlc-X7ervirfO8slpq3c
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID =
+    PRIVATE_GROUP_BOT_API_ID =-837164004
     # command handler
     COMMAND_HAND_LER = "."
     VCMODE = "True"
@@ -22,6 +22,6 @@ class Development(Config):
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
     # External plugins repo
-    EXTERNAL_REPO = "https://github.com/rishabhanand2/tha_plugins"
+    EXTERNAL_REPO = "https://github.com/shivamelu/tha_plugins"
     #don't edit this 
     THANOSABUSE = "False"#don't edit this else error in 
